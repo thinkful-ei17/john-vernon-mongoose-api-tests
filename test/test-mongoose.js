355db6551afa03f793257ebd2238ee45192f4a14
John Castillo <JohnluisCastillo@gmail.com>
2018-01-10 11:39:26 -0500

    index

---
+++ test/test-mongoose.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+const should = chai.should();
+
+const { BlogPost } = require('../models');
+const { closeServer, runServer, app } = require('../server');
+const { TEST_DATABASE_URL } = require('../config');
+
+chai.use(chaiHttp);
