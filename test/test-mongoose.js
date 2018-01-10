13d6e615e4cddb059898c601f8c9e80ef70ff01c
John Castillo <JohnluisCastillo@gmail.com>
2018-01-10 11:55:32 -0500

    added get testing

--- test/test-mongoose.js
+++ test/test-mongoose.js
@@ -12,3 +12,21 @@ const { closeServer, runServer, app } = require('../server');
 const { TEST_DATABASE_URL } = require('../config');
 
 chai.use(chaiHttp);
+
+
+describe('GET endpoint',function(){
+    it('should return all posts',function(){
+        let res;
+        return chai.request(app)
+        .get('/posts')
+        .then( _res => {
+            res = _res;
+            expect(res).should.have.status(200);
+            expect(res.body.post).to.have.length.of(1);
+            return BlogPost.count();
+        })
+        .then(count => {
+            expect(res.body.post).to.have.length.of(count);
+        })
+    })
+})
